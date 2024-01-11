f000a400af8e043aa7828fecdcb6f4e778696d05
Olulode Olatunbosun <horlathunbhosun@gmail.com>
2024-01-11 21:40:01 +0100

    removed comments

--- src/main/java/org/olatunbosun/Main.java
+++ src/main/java/org/olatunbosun/Main.java
@@ -17,21 +17,5 @@ public class Main {
 //        new WelcomeScreenGui();
 //           new ProfileGui();
 
-//        SwingUtilities.invokeLater(() -> {
-//            JFrame frame = new JFrame("Select Box Example");
-//            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//
-//            JPanel panel = new JPanel();
-//
-//            // Create an array of items for the JComboBox
-//            String[] items = {"Item 1", "Item 2", "Item 3", "Item 4", "Item 5"};
-//
-//            // Create a JComboBox and add items to it
-//
-//
-//            frame.setSize(300, 200);
-//            frame.setLocationRelativeTo(null); // Center the frame
-//            frame.setVisible(true);
-//        });
     }
 }
