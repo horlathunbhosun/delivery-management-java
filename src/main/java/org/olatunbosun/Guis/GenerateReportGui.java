56b4c2f32f20ca4b1b81213d9229f3efdb2f76a9
Olulode Olatunbosun <horlathunbhosun@gmail.com>
2024-01-30 11:04:03 +0100

    feat: started work on generate report gui

--- src/main/java/org/olatunbosun/Guis/GenerateReportGui.java
+++ src/main/java/org/olatunbosun/Guis/GenerateReportGui.java
@@ -1,4 +1,38 @@
 package org.olatunbosun.Guis;
 
-public class GenerateReportGui {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GenerateReportGui extends JFrame implements ActionListener {
+
+
+    public GenerateReportGui() {
+        super("Generate Report Page");
+
+        // Add the components to the frame
+        JPanel contentPane = new JPanel();
+
+        //add menu bar
+        MenuGui menuGui = new MenuGui();
+
+
+        setJMenuBar(menuGui);
+        setContentPane(contentPane);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 300);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+
+    /**
+     * Invoked when an action occurs.
+     *
+     * @param e the event to be processed
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
